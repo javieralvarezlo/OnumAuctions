79ea3b9987157d4ca3116ebafd708586f5bbef3e
Javier Álvarez <javieralvarez2.ja@gmail.com>
2024-10-04 14:33:37 +0200

    Get auctions

--- api/helpers.go
+++ api/helpers.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Auction struct {
-	AuctionID    string `json:"auctionId" bson:"-"`
+	AuctionID    string `json:"auctionId" bson:"_id,omitempty"`
 	StartValue   int64  `json:"startValue" bson:"startValue" binding:"required"`
 	BidStartTime int64  `json:"bidStartTime" bson:"bidStartTime" binding:"required"`
 	BidEndTime   int64  `json:"bidEndTime" bson:"bidEndTime" binding:"required"`
--- api/main.go
+++ api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func main() {
@@ -13,6 +17,8 @@ func main() {
 	router := gin.Default()
 
 	router.POST("/auctions", createAuction)
+
+	router.GET("/auctions", searchAuctions)
 
 	router.POST("/bids", createBid)
 
@@ -69,4 +75,63 @@ func createBid(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"bid": newBid,
 	})
+}
+
+func searchAuctions(c *gin.Context) {
+	from := c.DefaultQuery("from", fmt.Sprintf("%d", time.Now().Unix()))
+	to := c.DefaultQuery("to", fmt.Sprintf("%d", time.Unix(1<<63-62135596801, 999999999).Unix()))
+
+	fromInt, err := strconv.Atoi(from)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "From value is not valid",
+		})
+
+		return
+	}
+
+	toInt, err := strconv.Atoi(to)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "To value is not valid",
+		})
+
+		return
+	}
+
+	mongoClient := Connect()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	query := bson.M{
+		"bidStartTime": bson.M{"$gte": fromInt},
+		"bidEndTime":   bson.M{"$lte": toInt},
+	}
+	cursor, err := mongoClient.Database("auctions").Collection("auctions").Find(ctx, query)
+	failOnError(err, "Error fetching auctions")
+	defer cursor.Close(ctx)
+
+	var auctions []Auction
+	for cursor.Next(context.Background()) {
+		var current Auction
+		fmt.Println(current)
+		cursor.Decode(&current)
+		fmt.Println(11)
+		fmt.Println(current)
+		auctions = append(auctions, current)
+	}
+
+	if len(auctions) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "There are not auctions in this timeframe",
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"auctions": auctions,
+	})
+
 }
